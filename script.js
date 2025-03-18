fc088ae03411396426775c6e6a9335663ebe1e5c
Mahaveermohan <veer1298.mohan@gmail.com>
2025-03-18 18:41:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+    // Trim leading whitespace to ignore spaces before the first word
+    str = str.trim();
+    
+    // Find the index of the first space
+    let spaceIndex = str.indexOf(' ');
+    
+    // If there is no space, return the whole string
+    if (spaceIndex === -1) {
+        return str;
+    }
+    
+    // Return the substring up to the first space
+    return str.substring(0, spaceIndex);
 }
 
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// Test cases
+console.log(firstWord('see and stop')); // 'see'
+console.log(firstWord(' Hello World!')); // 'Hello'
+console.log(firstWord('12345')); // '12345'
+console.log(firstWord('')); // ''
+console.log(firstWord('   leading spaces test')); // 'leading'
+console.log(firstWord('singleWord')); // 'singleWord'
